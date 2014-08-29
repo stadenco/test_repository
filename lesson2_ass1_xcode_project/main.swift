0e654b5f52a8219bfdc5abd59b9392cebab68a41
Joel <stadenco@earthlink.net>
2014-08-28 21:40:13 -0400

    Initial Commit

---
+++ lesson2_ass1_xcode_project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lesson2_ass1_xcode_project
+//
+//  Created by joelschonbach on 8/28/14.
+//  Copyright (c) 2014 joelschonbach. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
